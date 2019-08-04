78c0a3eb0a603741ae0926a1bf68c1edbbdfc905
miyazaki <kattyan.08ms@gmail.com>
2019-08-04 11:51:01 +0900

    [add] App Review (apply grid system)

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -5,6 +5,7 @@ import ImageList from './imageList';
 
 class App extends React.Component {
   state = { images: [] };
+
   onSearchSubmit = async (term) => {
     const response = await unsplash.get('/search/photos', {
       params: { query: term },
