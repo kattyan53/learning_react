c9467effa2ec5762fc04b98a67a9de5a7ab34725
miyazaki <kattyan.08ms@gmail.com>
2019-08-07 08:46:57 +0900

    [add] create youtube api

--- videos/src/apis/youtube.js
+++ videos/src/apis/youtube.js
@@ -1,1 +1,12 @@
+import axios from 'axios';
+
 const KEY = 'AIzaSyBqViwj_Kg9Ohh0WbX_O8zUkEhI7KqukVM';
+
+export default axios.create({
+  baseURL: 'https//www.googleapis.com/youtube/v3',
+  params: {
+    part: 'snippet',
+    maxResults: 5,
+    key: KEY,
+  }
+});
