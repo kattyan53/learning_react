9ec1c73443680e25680fa44c1c1b554d24dda813
miyazaki <kattyan.08ms@gmail.com>
2019-08-08 08:54:58 +0900

    [add] Add Video Item Components

--- videos/src/components/VideoList.js
+++ videos/src/components/VideoList.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import VideoItem from './VideoItem';
 
-const VideoList = (props) => {
-  return <div>{props.videos.length}</div>;
+const VideoList = ( { videos } ) => {
+  const renderedList = videos.map(video => {
+    return <VideoItem video={video} />;
+  });
+
+  return <div className="ui relaxed deveded list">{renderedList}</div>;
 };
 
 export default VideoList;
