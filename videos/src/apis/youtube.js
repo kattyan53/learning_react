68893a6316c2256741e1d09deda1d7fdad238a2a
miyazaki <kattyan.08ms@gmail.com>
2019-08-07 09:15:07 +0900

    [add] Can communicate using youtube API

--- videos/src/apis/youtube.js
+++ videos/src/apis/youtube.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const KEY = 'AIzaSyBqViwj_Kg9Ohh0WbX_O8zUkEhI7KqukVM';
 
 export default axios.create({
-  baseURL: 'https//www.googleapis.com/youtube/v3',
+  baseURL: 'https://www.googleapis.com/youtube/v3',
   params: {
     part: 'snippet',
     maxResults: 5,
