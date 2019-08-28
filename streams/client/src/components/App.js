e276e6aa8d32f78bd334ed09886cb2e168e9fe77
miyazaki <kattyan.08ms@gmail.com>
2019-08-28 10:12:57 +0900

    [add] chapter16

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -1,30 +1,24 @@
 import React from 'react';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
+import StreamCreate from './streams/StreamCreate.js';
+import StreamDelete from './streams/StreamDelete.js';
+import StreamEdit from './streams/StreamEdit.js';
+import StreamList from './streams/StreamList.js';
+import StreamShow from './streams/StreamShow.js';
+import Header from './Header.js';
 
-const PageOne = () => {
-  return (
-    <div>
-      PageOne
-      <Link to="/pagetwo">Navigate to Page Two</Link>
-    </div>
-  );
-};
-
-const PageTwo = () => {
-  return (
-    <div>
-      <Link to="/">go to page one</Link>
-    </div>
-  );
-};
 
 const App = () => {
   return (
-    <div>
+    <div className="ui container">
       <BrowserRouter>
         <div>
-          <Route path="/" exact component={PageOne} />
-          <Route path="/pagetwo" component={PageTwo} />
+          <Header />
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" exact component={StreamCreate} />
+          <Route path="/streams/edit" exact component={StreamEdit} />
+          <Route path="/streams/delete" exact component={StreamDelete} />
+          <Route path="/streams/show" exact component={StreamShow} />
         </div>
       </BrowserRouter>
     </div>
